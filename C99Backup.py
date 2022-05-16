62362c06560f80eafeb83ff77edb99bc9ba88831
Suhanimathur1208 <81302633+Suhanimathur1208@users.noreply.github.com>
2022-05-16 13:16:52 +0530

    Add files via upload

---
+++ C99Backup.py
@@ -0,0 +1,8 @@
+import os 
+#os.system("date")
+#os.mkdir("C:/Users/Hemant mathur/Desktop/self game/newFolder")
+#os.getcwd()
+path='C:/Users/Hemant mathur/Desktop/self game'
+#isExist=os.path.exists(path)
+#print(isExist)
+os.listdir()
